457437da39e8e87175d2074a4e7acb90d2a07c16
JORDAN\cocdr <nutkoko675@gmail.com>
2025-09-05 19:39:27 +0800

    1st commit

---
+++ includes/topbar.php
@@ -0,0 +1,20 @@
+<div class="container-fluid bg-primary text-white d-none d-lg-flex wow fadeIn" data-wow-delay="0.1">
+    <div class="container py-3">
+        <div class="d-flex align-items-center">
+            <a href="index.html" class="d-flex align-items-center text-decoration-none">
+                <img src="img/srclogo.png" alt="SRC Logo" 
+                     class="me-2" style="height:50px; width:auto;">
+                <h2 class="text-white fw-bold m-0">Santa Rita College of Pampanga</h2>
+            </a>
+            <div class="ms-auto d-flex align-items-center">
+                <small class="ms-4"><i class="fa fa-map-marker-alt me-3"></i>San Jose Santa Rita Pampanga</small>
+                <small class="ms-4"><i class="fa fa-envelope me-3"></i>srcpampanga@gmail.com</small>
+                <small class="ms-4"><i class="fa fa-phone-alt me-3"></i>(045) 900 0557</small>
+                <div class="ms-3 d-flex">
+                    <a class="btn btn-sm-square btn-light text-primary ms-2" href=""><i
+                            class="fab fa-facebook-f"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
